0e2b5567f52a7052f9a18a3b2de59604230ca98f
yahong2018 <SoftwareChina@qq.com>
2019-11-14 21:29:18 +0800

    存储过程小错误

--- Imms/desgin/data/sql/procedures/Session/MES_DisplayMenu.sql
+++ Imms/desgin/data/sql/procedures/Session/MES_DisplayMenu.sql
@@ -2,7 +2,7 @@ create procedure MES_DisplayMenu(
   out    RespData              varchar(200)
 )
 begin
-  set RespData = '|2|1|4';
+  set RespData = '2|1|4';
   set RespData = CONCAT(RespData,'|210|128|129|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|1|150');
   set RespData = CONCAT(RespData,'|1|请选择功能：|0');	 
   set RespData = CONCAT(RespData,'|2|1.工件退回|0');	 
