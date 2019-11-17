89220715f96e35146db1ae5742f486f684cca375
yahong2018 <SoftwareChina@qq.com>
2019-11-17 10:41:28 +0800

    完善根据BOM自动报工

--- Imms/desgin/data/sql/procedures/MES_WipCardInput/MES_ReportWip/MES_AjustStockByBom.sql
+++ Imms/desgin/data/sql/procedures/MES_WipCardInput/MES_ReportWip/MES_AjustStockByBom.sql
@@ -26,11 +26,15 @@ begin
        
     delete from bom_stock where workstation_id = WorkStationId;
     
+    -- 第1层的部品(非原材料)
     insert into bom_stock(workstation_id,production_id,qty,lvl)
        select WorkStationId,b.component_id,b.component_qty * Qty,1 
         from bom b 
       where b.material_id = ProductionId
-        and b.bom_status = 1;
+        and b.bom_status = 1
+        and exists(
+          select * from bom b1 where b1.material_id = b.component_id
+    );
 
     call MES_Debug('MES_AjustStockByBom   1');
 
@@ -39,10 +43,17 @@ begin
         set CurLevel = CurLevel + 1;
         insert into bom_stock(workstation_id,production_id,qty,lvl)
             select WorkstationId, b.component_id,b.component_qty * bs.qty,CurLevel
-               from bom b join bom_stock bs on bs.production_id = b.material_id
-               where b.bom_status = 1
-                 and bs.workstation_id = WorkStationId
-                 and bs.lvl = CurLevel-1;
+               from bom b,bom_stock bs,material m
+               where  bs.workstation_id = WorkStationId
+               	  and b.material_id     = bs.production_id                  
+                  and b.material_id     = m.record_id
+                  and bs.lvl            = CurLevel - 1
+                  and b.bom_status      = 1
+                  and m.auto_finished_progress  = 1
+                  and exists(
+                    select * from bom b1 where b1.material_id = b.component_id
+                  )                  
+                 ;
       
         if not exists(select * from bom_stock where lvl = CurLevel and workstation_id = WorkstationId ) then
             leave BreakWhile;
@@ -54,17 +65,9 @@ begin
         end if;       
     end while;
 
-    call MES_Debug('MES_AjustStockByBom   99');
+    call MES_Debug('MES_AjustStockByBom   99');               
     
-    delete bs from bom_stock bs
-      where bs.workstation_id = WorkstationId
-        and not exists(
-             select * from material m 
-                where m.auto_finished_progress = 1
-                 and m.record_id = bs.production_id
-    );
-
-    -- 1. 自动报工    
+    -- 1. 自动报工记录    
     insert into production_order_progress(
         production_order_id,production_order_no,production_id,production_code,production_name,
         workshop_id,workshop_code,workshop_name,
@@ -86,9 +89,8 @@ begin
          -1,'','',null,0,
          Now(),TimeOfOriginWork,ShiftId,bs.qty,CardQty
      from bom_stock bs join material m on bs.production_id = m.record_Id
-     where bs.workstation_id = WorkstationId;
-
-    call MES_Debug('MES_AjustStockByBom   100');
+     where bs.workstation_id = WorkstationId
+       and m.auto_finished_progress = 1;
 
     -- 2. 生产进度
     insert into product_summary (product_date,workshop_id,workshop_code,workshop_name,production_id,production_code,production_name,qty_good_0,qty_defect_0,qty_good_1,qty_defect_1)
@@ -96,30 +98,25 @@ begin
            bs.production_id,m.material_code,m.material_name,0,0,0,0
     from  bom_stock bs join material m on bs.production_id = m.record_id
     where bs.workstation_id = WorkstationId
+      and m.auto_finished_progress = 1
       and not exists(
         select * from product_summary ps
           where ps.production_id = bs.production_Id
             and ps.workshop_id = WorkshopId
             and ps.product_date = TimeOfOriginWork
-    );
-    
-    if ShiftId = 0 then
-        update product_summary ps,bom_stock bs 
-          set ps.qty_good_0 = ps.qty_good_0 + bs.qty
-        where ps.production_id = bs.production_id
-          and ps.product_date = TimeOfOriginWork
-          and ps.workshop_id = WorkshopId
-          and bs.workstation_id = WorkstationId;
-    else 
-        update product_summary ps,bom_stock bs 
-          set ps.qty_good_1 = ps.qty_good_1 + bs.qty
-        where ps.production_id = bs.production_id
-          and ps.product_date = TimeOfOriginWork
-          and ps.workshop_id = WorkshopId
-          and bs.workstation_id = WorkstationId;
-    end if;    
+    );    
 
-    -- 调整库存
+    update product_summary ps,bom_stock bs 
+      set ps.qty_good_0 = if(ShiftId = 0, (ps.qty_good_0 + bs.qty), ps.qty_good_0),
+          ps.qty_good_1 = if(ShiftId = 1, (ps.qty_good_1 + bs.qty), ps.qty_good_1)
+    where ps.production_id = bs.production_id
+      and ps.product_date = TimeOfOriginWork
+      and ps.workshop_id = WorkshopId
+      and bs.workstation_id = WorkstationId;
+    
+    --
+    -- 调整库存:所有的部品都需要进行库存的调整
+    --
     insert into material_stock(material_id,material_code,material_name,store_id,store_code,store_name,
                                qty_stock,qty_move_in,qty_back_in,qty_back_out,qty_consume_good,qty_consume_defect,qty_good,qty_defect,qty_move_out,
                                create_by_id,create_by_code,create_by_name,create_time,opt_flag)
@@ -130,14 +127,17 @@ begin
     where bs.workstation_id = WorkstationId
      and not exists(
         select * from material_stock ms 
-          where ms.material_id = bs.production_Id
+          where ms.material_id = bs.production_id
             and ms.store_id = WorkshopId
-    );    
-   
+    );
+    
     -- 产出与消耗
-    update material_stock ms join bom_stock bs on ms.material_id = bs.production_id
-       set ms.qty_good = ms.qty_good + bs.qty,
+    update material_stock ms , bom_stock bs, material m
+       set ms.qty_stock = if(m.auto_finished_progress = 0, (ms.qty_stock - bs.qty), 0),
+           ms.qty_good =  if(m.auto_finished_progress = 1, (ms.qty_good + bs.qty), ms.qty_good),
            ms.qty_consume_good = ms.qty_consume_good + bs.qty
-    where ms.store_id = WorkshopId
-      and bs.workstation_id = WorkstationId;    
+    where ms.material_id = bs.production_id
+      and ms.store_id = WorkshopId
+      and bs.production_id = m.record_id
+      and bs.workstation_id = WorkstationId;
 end;
